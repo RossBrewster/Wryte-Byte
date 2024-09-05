45eb243e3713bc67da351c6112b45db86c506f94
Ross Brewster <ross.brewster14@gmail.com>
2024-09-05 03:05:23 -0500

    Add frontend homepage.

---
+++ wryte-byte/src/App.tsx
@@ -0,0 +1,19 @@
+
+import { HomePage } from './pages/HomePage'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      </div>
+      <h1 className='text-blue'>Wryte-Byte</h1>
+      <div className="card">
+      </div>
+      <HomePage />
+    </>
+  )
+}
+
+export default App
